69b2496eb3c3ebe413216f2c54732b8917b8438a
Andrew <andrew.vinocur@gmail.com>
2018-05-25 15:42:55 -0700

    creates shallow render of app with note

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,5 +5,6 @@ import App from '../App';
 
 
 it('shows a comment box', () => {
-
+  // wrapped is the term used by enzyme docs but component is a reasonable substitute 
+  const wrapped = shallow(<App />);
 });
