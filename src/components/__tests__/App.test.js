21a5a134a18a1aba1098aab996e35f634f88fcc5
Andrew <andrew.vinocur@gmail.com>
2018-05-25 15:45:32 -0700

    adds notes and first expect block

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 
 it('shows a comment box', () => {
-  // wrapped is the term used by enzyme docs but component is a reasonable substitute 
+  // wrapped is the term used by enzyme docs but component is a reasonable substitute
   const wrapped = shallow(<App />);
+
+  // expect "wrapped" to contain a "CommentBox". This returns an array with all items that match CommentBox and since
+  // there should only be 1 CommentBox, this should equal 1
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
