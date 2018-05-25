1521364b5b9da6f574be67c1e18372611a107e93
Andrew <andrew.vinocur@gmail.com>
2018-05-25 15:05:39 -0700

    initial test set up to simulate dom with a div

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,3 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from '../App';
+
+
+it('shows a comment box', () => {
+  const div = document.create('div');
+
+  ReactDOM.render(<App />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
