5461737615117de0bc5285e25ac07639dbd6c92e
Andrew <andrew.vinocur@gmail.com>
2018-05-25 15:57:04 -0700

    refactors to use beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,19 +5,18 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapped;
 
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  // wrapped is the term used by enzyme docs but component is a reasonable substitute
-  const wrapped = shallow(<App />);
-
   // expect "wrapped" to contain a "CommentBox". This returns an array with all items that match CommentBox and since
   // there should only be 1 CommentBox, this should equal 1
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
