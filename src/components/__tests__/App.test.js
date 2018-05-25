48a35233c0d05209373d923ee297e14f6044df2f
Andrew <andrew.vinocur@gmail.com>
2018-05-25 15:41:29 -0700

    clears out old test and imports enzyme shallow for use with jest

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import App from '../App';
 
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
 
-  ReactDOM.render(<App />, div);
-
-  ReactDOM.unmountComponentAtNode(div);
 });
