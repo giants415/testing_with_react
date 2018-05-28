8bc8385079fa7e24d53659e26dcacfd0f95b83af
Andrew <andrew.vinocur@gmail.com>
2018-05-28 11:04:58 -0700

    corrected test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -28,10 +28,13 @@ it('has a text area that users can type in', () => {
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
 
-it('clears the input after submission', () => {
-  wrapped.find('form').simulate('submit', {
+it('clears the text area after form submission', () => {
+  wrapped.find('textarea').simulate('change', {
     target: { value: 'new comment' }
   });
+  wrapped.update();
+
+  wrapped.find('form').simulate('submit');
   wrapped.update();
 
   expect(wrapped.find('textarea').prop('value')).toEqual('');
