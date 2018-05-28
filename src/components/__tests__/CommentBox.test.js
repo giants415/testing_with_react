4ad583e82b46e81822052c0bd4b7202a68ffae50
Andrew <andrew.vinocur@gmail.com>
2018-05-28 11:09:46 -0700

    uses decribe block to group similar tests and scope beforeEach to DRY code

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,25 +18,26 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
   });
-  wrapped.update();
+  
+  it('has a text area that users can type in', () => {
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('clears the text area after form submission', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
   });
-  wrapped.update();
-  // expectation below guards against a false positive
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
+  it('clears the text area after form submission', () => {
+    // expectation below guards against a false positive
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
