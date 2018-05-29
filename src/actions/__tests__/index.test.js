2e194ef1e584d9e23e249b1a70d9884dfd0cc758
Andrew <andrew.vinocur@gmail.com>
2018-05-29 13:38:41 -0700

    boilerplate for testing actions

---
+++ src/actions/__tests__/index.test.js
@@ -0,0 +1,12 @@
+import { saveComment } from 'actions';
+import { SAVE_COMMENT } from 'actions/types';
+
+describe('saveComment', () => {
+  it('has the correct type', () => {
+
+  });
+
+  it('has the correct payload', () => {
+
+  });
+});
