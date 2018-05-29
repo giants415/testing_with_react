928a6e173d620a754f72180267af495018cdc992
Andrew <andrew.vinocur@gmail.com>
2018-05-29 13:40:29 -0700

    adds specs for running tests

--- src/actions/__tests__/index.test.js
+++ src/actions/__tests__/index.test.js
@@ -3,10 +3,14 @@ import { SAVE_COMMENT } from 'actions/types';
 
 describe('saveComment', () => {
   it('has the correct type', () => {
+    const action = saveComment();
 
+    expect(action.type).toEqual(SAVE_COMMENT);
   });
 
   it('has the correct payload', () => {
+    const action = saveComment('New Comment');
 
+    expect(action.payload).toEqual('New Comment');
   });
 });
