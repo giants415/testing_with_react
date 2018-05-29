1727b9a69c6514b0d83501f6e7577ef2570e2671
Andrew <andrew.vinocur@gmail.com>
2018-05-29 16:19:30 -0700

    brings in new imports and refactors store code

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 // props.children is a react construct - allows us to take component we just created and allows us to wrap other components
 
 export default ({ children, initialState = {}}) => {
+  const store = createStore(reducers, initialState);
+
   return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
       {children}
     </Provider>
   );
