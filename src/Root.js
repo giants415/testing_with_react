a7cb492ceb4e25377fc8e34a4f8bf13dbe272768
Andrew <andrew.vinocur@gmail.com>
2018-05-29 12:55:23 -0700

    moves comment to fix error

--- src/Root.js
+++ src/Root.js
@@ -3,10 +3,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
+// props.children is a react construct - allows us to take component we just created and allows us to wrap other components
+
 export default (props) => {
   return (
     <Provider store={createStore(reducers, {})}>
-      // props.children is a react construct - allows us to take component we just created and allows us to wrap other components
       {props.children}
     </Provider>
   );
