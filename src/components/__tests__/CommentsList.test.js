17151414c7dc2ddc9e0f5e430c06001643977fbf
Andrew <andrew.vinocur@gmail.com>
2018-05-29 13:50:42 -0700

    sets up outline for test but does not solve issue where there is no comments to test with

--- src/components/__tests__/CommentsList.test.js
+++ src/components/__tests__/CommentsList.test.js
@@ -4,6 +4,15 @@ import { mount } from 'enzyme';
 import CommentList from 'components/CommentList';
 import Root from 'Root';
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentList />
+    </Root>
+  );
+});
 
 it('creates one LI per comment', () => {
 
