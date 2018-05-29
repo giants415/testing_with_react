dbfb2169886a1f0af43f4503c8ae7df5cb390e29
Andrew <andrew.vinocur@gmail.com>
2018-05-29 16:55:19 -0700

    first portion

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -5,8 +5,13 @@ import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
   //Attempt to render the entire App
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
 
   // find the 'fetch comments' button and click it
-  
+
   //expect to find a list of comments
 });
