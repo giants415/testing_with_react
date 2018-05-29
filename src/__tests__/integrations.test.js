e3da25a847865cc30dfd83cac63f5f038c59c04b
Andrew <andrew.vinocur@gmail.com>
2018-05-29 16:51:52 -0700

    outline for tests that need to be written

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -2,3 +2,11 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Root from 'Root';
 import App from 'components/App';
+
+it('can fetch a list of comments and display them', () => {
+  //Attempt to render the entire App
+
+  // find the 'fetch comments' button and click it
+  
+  //expect to find a list of comments
+});
