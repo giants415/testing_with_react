8771b8d56dc778115d2d841d8ce70b603ea0735c
Andrew <andrew.vinocur@gmail.com>
2018-05-29 13:54:14 -0700

    creates test list of comments and passes them in to the provider

--- src/components/__tests__/CommentsList.test.js
+++ src/components/__tests__/CommentsList.test.js
@@ -7,8 +7,12 @@ import Root from 'Root';
 let wrapped;
 
 beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2']
+  };
+
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   );
