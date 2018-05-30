49d8308c6cbe02b024877afd84bd6274d54efb0d
Andrew <andrew.vinocur@gmail.com>
2018-05-29 17:05:39 -0700

    sets up moxios in beforeEach

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -5,7 +5,14 @@ import Root from 'Root';
 import App from 'components/App';
 
 beforeEach(() => {
-
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [
+      {name: 'Fetched #1'},
+      {name: 'Fetched #2'}
+    ]
+  });
 });
 
 afterEach(() => {
