5327946466255ec7cb346ab68a1cfc300da319ad
Andrew <andrew.vinocur@gmail.com>
2018-05-29 17:09:48 -0700

    removes comments

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -20,16 +20,13 @@ afterEach(() => {
 });
 
 it('can fetch a list of comments and display them', () => {
-  //Attempt to render the entire App
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   );
 
-  // find the 'fetch comments' button and click it
   wrapped.find('.fetch-comments').simulate('click');
 
-  //expect to find a list of comments
   expect(wrapped.find('li').length).toEqual(2);
 });
