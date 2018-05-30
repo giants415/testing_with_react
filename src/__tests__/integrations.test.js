7b4fdf4602d4d2cd765c6144f4f1d96849faee5a
Andrew <andrew.vinocur@gmail.com>
2018-05-29 17:02:52 -0700

    imports moxios and skeletons for before/after each

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios'
 import Root from 'Root';
 import App from 'components/App';
+
+beforeEach(() => {
+
+});
+
+afterEach(() => {
+
+});
 
 it('can fetch a list of comments and display them', () => {
   //Attempt to render the entire App
