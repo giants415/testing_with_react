41330242e5383a6dffb82d02b71655bc875154f2
Andrew <andrew.vinocur@gmail.com>
2018-05-30 16:10:04 -0700

    adds in auth code

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default (ChildComponent) => {
   class ComposedComponent extends Component {
+    // component just got rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    // our component just got updated
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
     render() {
       return <ChildComponent />;
     }
   }
-  
-  return ComposedComponent;
+
+  function mapStateToProps(state) {
+    return { auth: state.auth };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };
