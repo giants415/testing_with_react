7c415901e1aab7c112c07ae453191c139f61568b
Andrew <andrew.vinocur@gmail.com>
2018-06-01 12:58:03 -0700

    2nd step for adding response to payload and then dispatches action backthrough middlewares

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -5,4 +5,11 @@ export default ({ dispatch }) => next => action => {
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
+
+  // we want to wait for the promise to resolve (get its data)
+  // then create a new action with that data and dispatch it
+  action.payload.then(function(response) {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
