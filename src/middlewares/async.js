ce5162b5e9d064ecd94502e417140abff33b16a7
Andrew <andrew.vinocur@gmail.com>
2018-06-01 12:39:36 -0700

    step 1

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -2,4 +2,7 @@ export default ({ dispatch }) => next => action => {
   // check to see if the action has a promise on payload prop
   // if yes, wait for it to resolve
   // if no, send action to next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
 };
