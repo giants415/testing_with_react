cbe9f2ec7a225a9d66160b5bf0cfa0bfd7ef629a
Andrew <andrew.vinocur@gmail.com>
2018-06-01 15:53:51 -0700

    imports and adds stateValidator to middlewares

--- src/Root.js
+++ src/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 import reducers from 'reducers';
 
 // props.children is a react construct - allows us to take component we just created and allows us to wrap other components
@@ -10,7 +11,7 @@ export default ({ children, initialState = {}}) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(async)
+    applyMiddleware(async, stateValidator)
   );
 
   return (
