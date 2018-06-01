00daaeb671d8e818b0096d99854f51eb82e801dd
Andrew <andrew.vinocur@gmail.com>
2018-06-01 12:38:04 -0700

    notes for constructing middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,3 +1,5 @@
 export default ({ dispatch }) => next => action => {
-
+  // check to see if the action has a promise on payload prop
+  // if yes, wait for it to resolve
+  // if no, send action to next middleware
 };
